8a84ebd40a52116ddd7b88b7359a8fa53f5c6c53
ykadir42 <ykadir42@gmail.com>
2018-02-09 22:40:05 -0500

    Mathematical

--- draw.c
+++ draw.c
@@ -7,48 +7,72 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
-  if(x0 > x1){
-    int temp = x0;
-    x0 = x1;
-    x1 = temp;
-    temp = y0;
-    y0 = y1;
-    y1 = temp;
-  }
-  int x = x0;
-  int y = y0;
-  int a = y1 - y0;
-  int b = x0 - x1;
-
-  if(a > 0 && b < 0){
-    if(a + b < 0){
-      int d = 2 * a + b;
-      while (x < x1){
-	plot(s, c, x, y); 
-	if(d>0){ 
-	  y++;
-	  d += 2 * b;
+	if(x0 > x1){
+		int temp = x0;
+		x0 = x1;
+		x1 = temp;
+		temp = y0;
+		y0 = y1;
+		y1 = temp;
 	}
-	x++;
-	d += 2 * a;	
-      }
-    }
-  }
-  
-  else{
-    if(a + b < 0){
-      int d = 2 * a - b;
-      while (x < x1){
-	plot(s, c, x, y); 
-	if(d>0){ 
-	  y--;
-	  d += 2 * b;
-	}
-	x++;
-	d += 2 * a;
-      }
-    }
-  }
+	int x = x0;
+	int y = y0;
+	int a = y1 - y0;
+	int b = x0 - x1;
 
-  
+	if(a >= 0){
+		if(a + b <= 0){
+			int d = 2 * a + b;
+			while (x <= x1){
+				plot(s, c, x, y);
+				if(d > 0){
+					y++;
+					d += 2 * b;
+				}
+				x++;
+				d += 2 * a;
+			}
+		}
+		else{
+			int d = a + 2 * b;
+			while (y <= y1){
+				plot(s, c, x, y);
+				if(d < 0){
+					x++;
+					d += 2 * a;
+				}
+				y++;
+				d += 2 * b;
+			}
+		}
+	}
+
+	else{
+		if(b - a <= 0){
+			int d = 2 * a - b;
+			while (x <= x1){
+				plot(s, c, x, y);
+				if(d < 0){
+					y--;
+					d -= 2 * b;
+				}
+				x++;
+				d += 2 * a;
+			}
+		}
+		else{
+			int d = a - 2 * b;
+			while (y >= y1){
+				plot(s, c, x, y);
+				if(d > 0){
+					x++;
+					d += 2 * a;
+				}
+				y--;
+				d -= 2 * b;
+			}
+		}
+	}
+
+
 }
--- main.c
+++ main.c
@@ -8,17 +8,47 @@
 
 int main() {
 
-  screen s;
-  color c;
+	screen s;
+	color c;
 
-  c.red = 0;
-  c.green = MAX_COLOR;
-  c.blue = 0;
+	clear_screen(s);
 
-  clear_screen(s);
+	c.red = MAX_COLOR;
+	c.green = MAX_COLOR;
+	c.blue = MAX_COLOR / 2;
 
-  draw_line(0, 0, 500, 600, s, c);
+	float x, y;
+	int pixels = 500;
+	const float INTERVAL = 0.1;
+	for(x = -2; x <= 2; x += INTERVAL){
+		for(y = -2; y <= 2; y += INTERVAL){
+			if(x * x * x * x / 16 + y * y * y * y / 8 < 1){
+				float z = -3 * (x * x - y * y) / 4;
+				float partial_x = -3 * x / 2;
+				float partial_y = 3 * y / 2;
 
-  display(s);
-  save_extension(s, "lines.png");
+				//finding the position in the 2D picture given 3D coordinates.
+				//Slightly rotated counterclockwise for a better view.
+
+				if((x + INTERVAL) * (x + INTERVAL) * (x + INTERVAL) * (x + INTERVAL) / 16 + y * y * y * y / 8 < 1){
+					int col = (int)(pixels / 2 - y * 4 * pixels / 48 + x * 8 * pixels / 48);
+					int dest_col = (int)(pixels / 2 - y * 4 * pixels / 48 + (x + INTERVAL) * 8 * pixels / 48);
+					int row = (int)(pixels / 2 - z * pixels / 20 + y * pixels / 16 + x * pixels / 16);
+					int dest_row = (int)(pixels / 2 - (z + partial_x * INTERVAL) * pixels / 20 + y * pixels / 16 + (x + INTERVAL) * pixels / 16);
+					draw_line(col, row, dest_col, dest_row, s, c);
+				}
+
+				if(x * x * x * x / 16 + (y + INTERVAL) * (y + INTERVAL) * (y + INTERVAL) * (y + INTERVAL) / 8 < 1){
+					int col = (int)(pixels / 2 - y * 4 * pixels / 48 + x * 8 * pixels / 48);
+					int dest_col = (int)(pixels / 2 - (y + INTERVAL) * 4 * pixels / 48 + x * 8 * pixels / 48);
+					int row = (int)(pixels / 2 - z * pixels / 20 + y * pixels / 16 + x * pixels / 16);
+					int dest_row = (int)(pixels / 2 - (z + partial_y * INTERVAL) * pixels / 20 + (y + INTERVAL) * pixels / 16 + x * pixels / 16);
+					draw_line(col, row, dest_col, dest_row, s, c);
+				}
+			}
+		}
+	}
+
+	display(s);
+	save_extension(s, "lines.png");
 }
